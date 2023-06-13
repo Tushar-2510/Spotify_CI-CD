ff3e58b7a9aadd4c968e6706d793085006803a47
Tushar-2510 <77832400+Tushar-2510@users.noreply.github.com>
2023-06-13 11:32:29 +0530

    Update spotify.sh

--- spotify.sh
+++ spotify.sh
@@ -1,6 +1,6 @@
 TOKEN=$(curl -X POST "https://accounts.spotify.com/api/token" \
      -H "Content-Type: application/x-www-form-urlencoded" \
-     -d "grant_type=client_credentials&client_id=CLIENT_ID&client_secret=CLIENT_SECRET" | jq -r '. | .access_token')
+     -d "grant_type=client_credentials&client_id=${{ secrets.Client_id }}&client_secret=${{ secrets.Client_secret }}" | jq -r '. | .access_token')
 
 ARTIST=$(curl "https://api.spotify.com/v1/artists/4Z8W4fKeB5YxbusRsdQVPb" \
      -H "Authorization: Bearer $TOKEN" | jq -r '.images[0].url')
